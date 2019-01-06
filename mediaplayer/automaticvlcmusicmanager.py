ee5b8f4744dbcd22380a0c0ef4fe5a041fa2b572
Michael Yeiter <Aceplace1116@gmail.com>
2019-01-06 08:14:40 -0500

    Added to automatically load songs.

---
+++ mediaplayer/automaticvlcmusicmanager.py
@@ -0,0 +1,29 @@
+import re
+import threading
+import time
+
+from mediaplayer.vlcmusicmanager import VLCMusicManager
+
+
+class AutomaticVLCMusicManager(VLCMusicManager):
+    def __init__(self, vlc_connection, get_song_callback):
+        VLCMusicManager.__init__(self, vlc_connection)
+        self.get_song_callback = get_song_callback
+        threading.Thread(target=self.check_for_adding_song, daemon=True).start()
+
+
+    def check_for_adding_song(self):
+        while True:
+            time.sleep(1.5)
+            self.send_vlc_message('status')
+
+
+    def receive_vlc_messages(self, message):
+        VLCMusicManager.receive_vlc_messages(self, message)
+        match = re.search(r' stop state: 5 ', message)
+        if match and self.get_song_callback:
+            new_song = self.get_song_callback()
+            self.send_vlc_message(f'add "{new_song}"')
+
+
+
